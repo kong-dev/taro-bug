1c71e6ac76938d422c610e411e87f213d30bd163
kongwh <piaoxyx@gmail.com>
2021-08-06 18:54:41 +0800

    new commit

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,3 +1,7 @@
+export const foo = <const>{
+  bar: 'test'
+}
+
 export default {
   pages: [
     'pages/index/index'
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import { Component } from 'react'
 import './app.scss'
+import { foo } from './app.config'
 
 class App extends Component {
 
-  componentDidMount () {}
+  componentDidMount () {
+    console.log('foo', foo)
+  }
 
   componentDidShow () {}
 
